7274264c563ead969766e8c236f21731d559d5f9
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-05 08:23:13 +0700

    Add project files.

---
+++ Demo01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo01
+{
+    //Blind 75
+    //https://leetcode.com/problem-list/v9c2aln6/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        //20. Valid Parentheses
+        public void ValidParentheses ()
+        {
+
+        }
+
+
+
+
+    }
+}
