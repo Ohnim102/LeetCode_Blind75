98a346e891995ae44d890a72cfdecf61bf8e4df6
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-07 12:33:44 +0700

    21. Merge Two Sorted Lists

---
+++ Demo01/_21_MergeTwoSortedLists.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    public class _21_MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            var dummy = new ListNode(-1); // node giả
+            var current = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    current.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+
+            // Gắn nốt phần còn lại
+            if (list1 != null) current.next = list1;
+            if (list2 != null) current.next = list2;
+
+            return dummy.next; // bỏ node giả đi
+        }
+
+        public ListNode CreateListNode(List<int> listItem)
+        {
+            if (listItem == null || listItem?.Count() == 0) return null;
+
+            // tạo node head
+            var head = new ListNode(listItem[0]);
+            var current = head;
+
+            // duyệt từ phần tử thứ 2 trở đi
+            for (int i = 1; i < listItem.Count; i++)
+            {
+                current.next = new ListNode(listItem[i]);
+                current = current.next;
+            }
+
+            return head;
+        }
+
+        public bool AreEqual(ListNode l1, ListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val != l2.val) return false;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            // Nếu cả hai cùng null thì giống hệt
+            return l1 == null && l2 == null;
+        }
+
+    }
+
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Test/Test_21_MergeTwoSortedLists.cs
@@ -0,0 +1,91 @@
+﻿using Demo01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class Test_21_MergeTwoSortedLists
+    {
+        [TestMethod]
+        public void TestEx01()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+            ListNode listNodeResult = ex.CreateListNode(new List<int>() { 1, 1, 2, 3, 4, 4 });
+
+            var rs = ex.MergeTwoLists(listNode1, listNode2);
+            Assert.IsTrue(ex.AreEqual(rs, listNodeResult));
+        }
+        
+        [TestMethod]
+        public void TestEx02()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = null;
+            ListNode listNode2 = null;
+            ListNode listNodeResult = ex.CreateListNode(new List<int>() {  });
+
+            var rs = ex.MergeTwoLists(listNode1, listNode2);
+            Assert.IsTrue(ex.AreEqual(rs, listNodeResult));
+        }
+
+        [TestMethod]
+        public void TestEx03()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = ex.CreateListNode(new List<int>() { 1 }); ;
+            ListNode listNode2 = null;
+            ListNode listNodeResult = ex.CreateListNode(new List<int>() { 1 });
+
+            var rs = ex.MergeTwoLists(listNode1, listNode2);
+            Assert.IsTrue(ex.AreEqual(rs, listNodeResult));
+        }
+
+        [TestMethod]
+        public void TestEx04()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = ex.CreateListNode(new List<int>() { -9, 3 });
+            ListNode listNode2 = ex.CreateListNode(new List<int>() { 5, 7 });
+            ListNode listNodeResult = ex.CreateListNode(new List<int>() { -9, 3, 5, 7 });
+
+            var rs = ex.MergeTwoLists(listNode1, listNode2);
+            Assert.IsTrue(ex.AreEqual(rs, listNodeResult));
+        }
+        
+        [TestMethod]
+        public void TestEx05()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = ex.CreateListNode(new List<int>() { -10, -9, -6, -4, 1, 9, 9 });
+            ListNode listNode2 = ex.CreateListNode(new List<int>() { -5, -3, 0, 7, 8, 8 });
+            ListNode listNodeResult = ex.CreateListNode(new List<int>() { -10, -9, -6, -5, -4, -3, 0, 1, 7, 8, 8, 9, 9 });
+
+            var rs = ex.MergeTwoLists(listNode1, listNode2);
+            Assert.IsTrue(ex.AreEqual(rs, listNodeResult));
+        }
+
+        [TestMethod]
+        public void TestEx()
+        {
+            var ex = new _21_MergeTwoSortedLists();
+
+            ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            var listNode2 = ex.CreateListNode(new List<int>() { 1, 2, 4 });
+
+            Assert.IsTrue(true);
+        }
+
+
+    }
+}
