7ab7767b777644904a8c88874f80362e07c5ddda
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-08 09:28:53 +0700

    100. Same Tree

---
+++ Demo01/_100_SameTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    public class _100_SameTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+
+            // Recursively check if the left and right subtrees are identical
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Test/Test_100_SameTree.cs
@@ -0,0 +1,25 @@
+﻿using Demo01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class Test_100_SameTree
+    {
+        [TestMethod]
+        public void TestEx01()
+        {
+            var ex = new _100_SameTree();
+
+            TreeNode p = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+            TreeNode q = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+            var rs = ex.IsSameTree(p, q);
+            Assert.IsTrue(rs);
+        }
+    }
+}
