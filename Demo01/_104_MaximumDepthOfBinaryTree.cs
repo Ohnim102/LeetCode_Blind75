cf026c175fab21e31b5658f3af07e9e6021a55d5
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-10 08:33:03 +0700

    104. Maximum Depth of Binary Tree

---
+++ Demo01/_104_MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01._104_MaximumDepthOfBinaryTree
+{
+    public class _104_MaximumDepthOfBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int maxLeft = MaxDepth(root.left);
+            int maxRight = MaxDepth(root.right);
+            return Math.Max(maxLeft, maxRight) + 1;
+        }
+
+
+        //[3,9,20,null,null,15,7]
+        public TreeNode CreateListNode(List<int> listItem)
+        {
+            if (listItem == null || listItem?.Count() == 0) return null;
+
+            // tạo node head
+            var head = new TreeNode();
+            var current = head;
+
+            // duyệt từ phần tử thứ 2 trở đi
+            for (int i = 1; i < listItem.Count; i = i + 2)
+            {
+                current.val = listItem[i];
+
+                current.left = new TreeNode(listItem[i + 1]);
+                current.right = new TreeNode(listItem[i + 2]);
+            }
+
+            return head;
+        }
+    }
+
+
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
---
+++ Test/Test_104_MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using Demo01._104_MaximumDepthOfBinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class Test_104_MaximumDepthOfBinaryTree
+    {
+        [TestMethod]
+        public void TestEx01()
+        {
+            var ex = new _104_MaximumDepthOfBinaryTree();
+
+            //[3,9,20,null,null,15,7]
+            TreeNode input = new TreeNode(
+                3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7))
+            );
+
+            var rs = ex.MaxDepth(input);
+            Assert.AreEqual(3, rs);
+
+            //TreeNode p = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+            //TreeNode q = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+            //var rs = ex.IsSameTree(p, q);
+            //Assert.IsTrue(rs);
+        }
+
+    }
+}
