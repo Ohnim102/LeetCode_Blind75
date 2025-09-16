57e513a13d135208361ff8955425a45040856134
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-16 10:33:25 +0700

    217. Contains Duplicate Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

---
+++ Demo01/_217_ContainsDuplicate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    public class _217_ContainsDuplicate
+    {
+        /// <summary>
+        /// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool ContainsDuplicate(int[] nums)
+        {
+            ////Approach 1: Brute Force
+            //foreach (var i in nums)
+            //{
+            //    if (nums.Count(x => x == i) > 1) return true;
+            //}
+            //return false;
+
+
+            ////Approach 2: Sorting
+            //nums = nums.OrderBy(x => x).ToArray();
+            //for (int i = 1; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == nums[i - 1]) return true;
+            //}
+            //return false;
+
+
+            ////Approach 3: Hash Set
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (hashSet.Contains(num)) return true;
+                hashSet.Add(num);
+            }
+            
+            return false;
+
+        }
+    }
+}
---
+++ Test/Test_217_ContainsDuplicate.cs
@@ -0,0 +1,34 @@
+﻿using Demo01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass] 
+    public class Test_217_ContainsDuplicate
+    {
+        [TestMethod]
+        public void TestEx01()
+        {
+            var ex = new _217_ContainsDuplicate();
+            Assert.IsTrue(ex.ContainsDuplicate([1, 2, 3, 1]));
+        }
+
+        [TestMethod]
+        public void TestEx02()
+        {
+            var ex = new _217_ContainsDuplicate();
+            Assert.IsFalse(ex.ContainsDuplicate([1, 2, 3, 4]));
+        }
+
+        [TestMethod]
+        public void TestEx03()
+        {
+            var ex = new _217_ContainsDuplicate();
+            Assert.IsTrue(ex.ContainsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+        }
+    }
+}
