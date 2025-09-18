c31c80d566e2b52e2b680a20e154a36baaf85d1b
Doan Ngoc Minh <minhdn@aits.vn>
2025-09-18 08:52:59 +0700

    242. Valid Anagram. Given two strings s and t, return true if t is an anagram of s, and false otherwise.

--- Demo01/Blind70.cs
+++ Demo01/Blind70.cs
@@ -8,6 +8,49 @@ namespace Demo01
 {
     public class Blind70
     {
+        /// <summary>
+        /// 242. Valid Anagram. 
+        /// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool IsAnagram(string s, string t)
+        {
+            ////Approach 1: Brute Force
+            //if (s.Length != t.Length) return false;
+            //foreach (var c in s)
+            //{
+            //    if (t.Contains(c))
+            //    {
+            //        t = t.Remove(t.IndexOf(c), 1);
+            //        s = s.Remove(s.IndexOf(c), 1);
+            //    }
+            //    else return false;
+            //}
+
+            //if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t)) return true;
+            //return false;
+
+
+            ////Approach 2: Count c
+            var count = new Dictionary<char, int>();
+            if (s.Length != t.Length) return false;
+            foreach (var c in s)
+            {
+                if (!count.ContainsKey(c)) count[c] = 0;
+                count[c]++;
+            }
+
+            foreach (var c in t)
+            {
+                if (!count.ContainsKey(c)) return false;
+                count[c]--;
+                if (count[c] < 0) return false;
+            }
+            return true;
+        }
+
 
     }
 }
--- Test/TestBlind70.cs
+++ Test/TestBlind70.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,16 @@ namespace Test
     [TestClass]
     public class TestBlind70
     {
+        [TestMethod]
+        public void TestIsAnagram01()
+        {
+            Assert.IsTrue(Blind70.IsAnagram("anagram", "nagaram"));
+        }
 
+        [TestMethod]
+        public void TestIsAnagram02()
+        {
+            Assert.IsFalse(Blind70.IsAnagram("ab", "a"));
+        }
     }
 }
