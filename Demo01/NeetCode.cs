494a73699949b4efb2ed93691205a315a17535ee
Doan Ngoc Minh <minhdn@aits.vn>
2025-10-02 09:59:45 +0700

    1. Two Sum
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    You can return the answer in any order.

--- Demo01/NeetCode.cs
+++ Demo01/NeetCode.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Demo01
 {
@@ -55,6 +58,30 @@ namespace Demo01
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// 1. Two Sum
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        /// You can return the answer in any order.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> hashSet = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int other = target - nums[i];
+                if (hashSet.ContainsKey(other))
+                {
+                    return new int[2] { i, hashSet[other] };
+                }
+                hashSet[nums[i]] = i;
+            }
+            throw new Exception("No solution found");
         }
 
 
